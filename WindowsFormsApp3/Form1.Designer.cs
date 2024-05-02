1e22bf31414834e25e60c79106b99e9cf8499027
Furkan Demir <139778670+fd2955@users.noreply.github.com>
2024-05-02 21:22:53 +0300

    Initial commit

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTahmin = new System.Windows.Forms.TextBox();
+            this.btnTahmin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(31, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tahmininizi Giriniz (1-100)";
+            // 
+            // txtTahmin
+            // 
+            this.txtTahmin.Location = new System.Drawing.Point(90, 103);
+            this.txtTahmin.Name = "txtTahmin";
+            this.txtTahmin.Size = new System.Drawing.Size(175, 22);
+            this.txtTahmin.TabIndex = 1;
+            this.txtTahmin.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtTahmin_KeyUp);
+            // 
+            // btnTahmin
+            // 
+            this.btnTahmin.Location = new System.Drawing.Point(133, 156);
+            this.btnTahmin.Name = "btnTahmin";
+            this.btnTahmin.Size = new System.Drawing.Size(88, 30);
+            this.btnTahmin.TabIndex = 2;
+            this.btnTahmin.Text = "Tahmin Et";
+            this.btnTahmin.UseVisualStyleBackColor = true;
+            this.btnTahmin.Click += new System.EventHandler(this.btnTahmin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 315);
+            this.Controls.Add(this.btnTahmin);
+            this.Controls.Add(this.txtTahmin);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Sayı tahmin oyunu";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTahmin;
+        private System.Windows.Forms.Button btnTahmin;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int rnd;
+        int tahminSayisi = 0;
+        Random r;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            r = new Random();
+            rnd = r.Next(1, 101);
+        }
+
+        private void btnTahmin_Click(object sender, EventArgs e)
+        {
+            tahminSayisi++;
+
+            int tahmin = int.Parse(txtTahmin.Text);
+
+            if (tahmin == rnd)
+            {
+                var cevap = MessageBox.Show($"Doğru tahmin ettin kerata {tahminSayisi} denemede bildin. tekrar oyna la", "Tebrikler", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (cevap == DialogResult.No)
+                {
+                    this.Close();
+                }
+                else if (cevap == DialogResult.Yes)
+                {
+                    rnd = r.Next(1, 101);
+                    tahminSayisi = 0;
+                }
+            }
+            else if (tahmin > rnd)
+            {
+                MessageBox.Show("daha küçük sayı gir");
+
+            }
+            else if (tahmin < rnd)
+            {
+                MessageBox.Show("Daha büyük sayı gir");
+            }
+            txtTahmin.Text = "";
+            txtTahmin.Focus();
+
+        }
+
+        private void txtTahmin_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnTahmin.PerformClick();
+            }
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            txtTahmin.Focus();
+        }
+    }
+}
